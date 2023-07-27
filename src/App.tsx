b48673777234f75905916a15318e0de73d6bf7f3
Chris Deck <deckchris95@gmail.com>
2023-07-27 17:05:51 -0400

    initial start

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+import About from './components/about';
+import Portfolio from './components/portfolio';
+import Testimonials from  './components/testimonials';
+import Contact from './components/contact';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <About/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/about.tsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+  render() {
+    //let resumeData = this.props.resumeData;
+    return (
+      <section id="about">
+         <div className="row">
+            <div className="three columns">
+               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+            </div>
+            <div className="nine columns main-col">
+               <h2>About Me</h2>
+               <p>
+               {/* {
+                 resumeData.aboutme
+               } */}
+               </p>
+
+               <div className="row">
+
+                  <div className="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p className="address">
+       						<span>name</span>
+                     <br></br>
+       						   <span>
+                     address
+                    </span>
+                    <br></br>
+                    <span>website</span>
+       					   </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+    );
+  }
+}
